25331e265e8f0b1f2be951826672332342edf63a
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-07-07 13:31:36 +0530

    production done, ready to deploy

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dontenv = require('dotenv');
 const colors = require('colors');
@@ -20,7 +21,11 @@ if(process.env.NODE_ENV === 'development'){
 
 app.use('/api/v1/transactions', transactions)
 
-app.get('/',(req,res) => res.send('Hello'));
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+  }
 
 const PORT = process.env.PORT || 5000;
 
